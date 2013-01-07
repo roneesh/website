c10fccb55353451d34583eeadba6cdce671b1c98
Roneesh Vashisht <roneesh@gmail.com>
2013-01-06 18:50:44 -0600

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def shop
+  end
+
+  def portfolio
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def twitter
+  end
+
+  def writing
+  end
+
+  def blog
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roneeshwebsite::Application.config.secret_token = '1a9d71cbb58c587757d86344b37912d411474c6c855927eabe73ac4a2e4d191be582d0c38411d55f458bc5d7c1cc0cd69f4ea7c29908b400364f50c5f42f11e6'
