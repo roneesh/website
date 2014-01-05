e4fde30da83310b7cbeecc2f9b52f1eb4f3feb27
Roneesh Vashisht <roneesh@gmail.com>
2014-01-05 16:15:10 -0600

    commenting out amazon from initializer

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -6,8 +6,8 @@ Twitter.configure do |config|
 end
 
 
-AWS::S3::Base.establish_connection!(  
-  :access_key_id => ENV["AMAZON_ACCESS_KEY_ID"],
-  :secret_access_key => ENV["AMAZON_SECRET_ACCESS_KEY"])
+# AWS::S3::Base.establish_connection!(  
+#   :access_key_id => ENV["AMAZON_ACCESS_KEY_ID"],
+#   :secret_access_key => ENV["AMAZON_SECRET_ACCESS_KEY"])
 
-BUCKET = 'roneeshwebsite-lessons'
+# BUCKET = 'roneeshwebsite-lessons'
